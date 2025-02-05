2c8fd3477088df961d7acbc679cd1629601f54e4
hakos47 <ismaelguizan5@gmail.com>
2025-02-05 14:01:50 +0100

    add: var bidding

--- app/src/main/java/com/vango/presentation/auth/accessAccount/ActivityAccessAccount.kt
+++ app/src/main/java/com/vango/presentation/auth/accessAccount/ActivityAccessAccount.kt
@@ -6,10 +6,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.vango.R
+import com.vango.databinding.ActivityAccessAccountBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class ActivityAccessAccount : AppCompatActivity() {
+    private var binding: ActivityAccessAccountBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
