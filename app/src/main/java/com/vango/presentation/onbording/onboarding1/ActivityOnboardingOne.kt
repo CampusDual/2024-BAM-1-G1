b4710a960b86f538d263009e377a649555419acc
DESKTOP-2CUV67N\Lorena <lorenalopezvazquez@hotmail.com>
2025-02-07 03:56:02 +0100

    fix: activitys and xml

--- app/src/main/java/com/vango/presentation/onbording/onboarding1/ActivityOnboardingOne.kt
+++ app/src/main/java/com/vango/presentation/onbording/onboarding1/ActivityOnboardingOne.kt
@@ -1,4 +1,4 @@
-package com.vango.presentation.auth.login
+package com.vango.presentation.onbording.onboarding1
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
@@ -7,15 +7,15 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.vango.R
 
-class ActivityLogin : AppCompatActivity() {
+class ActivityOnboardingOne : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_onboarding_one)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
     }
 }
