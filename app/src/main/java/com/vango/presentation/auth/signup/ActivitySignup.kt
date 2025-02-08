6412041212787e0d925379bff0c8abd94cd3fb46
hakos47 <ismaelguizan5@gmail.com>
2025-02-08 12:23:48 +0100

    feature: navidate link logion

--- app/src/main/java/com/vango/presentation/auth/signup/ActivitySignup.kt
+++ app/src/main/java/com/vango/presentation/auth/signup/ActivitySignup.kt
@@ -1,18 +1,33 @@
 package com.vango.presentation.auth.signup
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.vango.R
+import com.vango.databinding.ActivitySignupBinding
+import com.vango.presentation.auth.login.ActivityLogin
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class ActivitySignup : AppCompatActivity() {
+    var binding: ActivitySignupBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_signup)
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+
+        val linkLogin = binding!!.tvNoLoginRegister
+        linkLogin?.setOnClickListener {
+            Log.e("signup", "click login")
+            val intent = Intent(this, ActivityLogin::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
 }
