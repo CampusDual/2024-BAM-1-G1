dadd7852e72517fd25be4046700b958d8a0db1a7
hakos47 <ismaelguizan5@gmail.com>
2025-02-08 11:39:36 +0100

    feature: fix reguster and strings

--- app/src/main/java/com/vango/presentation/auth/signup/ActivitySignup.kt
+++ app/src/main/java/com/vango/presentation/auth/signup/ActivitySignup.kt
@@ -14,10 +14,5 @@ class ActivitySignup : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_signup)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
