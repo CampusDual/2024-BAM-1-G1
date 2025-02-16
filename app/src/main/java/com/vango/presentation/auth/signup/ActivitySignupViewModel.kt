1423704d647fc5f912eee477de1ee632d2e0fa71
DESKTOP-2CUV67N\Lorena <lorenalopezvazquez@hotmail.com>
2025-02-16 16:58:11 +0100

    old sign up

--- app/src/main/java/com/vango/presentation/auth/signup/ActivitySignupViewModel.kt
+++ app/src/main/java/com/vango/presentation/auth/signup/ActivitySignupViewModel.kt
@@ -1,10 +1,22 @@
 package com.vango.presentation.auth.signup
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class ActivitySignupViewModel @Inject constructor() : ViewModel() {
+    private val _email: MutableLiveData<String> = MutableLiveData()
+    val email: MutableLiveData<String> = _email
+
+    private val _errorEmail: MutableLiveData<Boolean> = MutableLiveData()
+    val errorEmail: MutableLiveData<Boolean> = _errorEmail
+
+    fun setEmail(email: String) {
+        _email.value = email
+        _errorEmail.value = true
+    }
+
 
 }
