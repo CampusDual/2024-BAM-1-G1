b09704f7ee80eb4fc6bcaecc085f3ccfd5f5e89c
zYoelz <vip.yoel@gmail.com>
2025-03-04 20:25:21 +0100

    feat: Add ID properties to region enums and implement `fromId` functions
    - Adds an `id` property to `SpanishProvinces`, `PortugueseRegions`, and `FrenchRegions` enums.
    - Adds `fromId` companion functions to `SpanishProvinces`, `PortugueseRegions`, and `FrenchRegions` for lookup by ID.
    - Updates the existing enums with new ids.

--- app/src/main/java/com/vango/presentation/auth/profile/provinces.kt
+++ app/src/main/java/com/vango/presentation/auth/profile/provinces.kt
@@ -1,45 +1,57 @@
 package com.vango.presentation.auth.profile
 
-enum class SpanishProvinces {
-    ALAVA, ALBACETE, ALICANTE, ALMERIA, ASTURIAS, AVILA,
-    BADAJOZ, BARCELONA, BURGOS, CACERES, CADIZ, CANTABRIA,
-    CASTELLON, CEUTA, CIUDAD_REAL, CORDOBA, CUENCA, GIRONA,
-    GRANADA, GUADALAJARA, GUIPUZCOA, HUELVA, HUESCA, ISLAS_BALEARES,
-    JAEN, LA_RIOJA, LAS_PALMAS, LEON, LLEIDA, LUGO, MADRID,
-    MALAGA, MELILLA, MURCIA, NAVARRA, OURENSE, PALENCIA,
-    PONTEVEDRA, SALAMANCA, SANTA_CRUZ_DE_TENERIFE, SEGOVIA,
-    SEVILLA, SORIA, TARRAGONA, TERUEL, TOLEDO, VALENCIA,
-    VALLADOLID, VIZCAYA, ZAMORA, ZARAGOZA;
+enum class SpanishProvinces(val id: Int) {
+    ALAVA(1), ALBACETE(2), ALICANTE(3), ALMERIA(4), ASTURIAS(5), AVILA(6),
+    BADAJOZ(7), BARCELONA(8), BURGOS(9), CACERES(10), CADIZ(11), CANTABRIA(12),
+    CASTELLON(13), CEUTA(14), CIUDAD_REAL(15), CORDOBA(16), CUENCA(17), GIRONA(18),
+    GRANADA(19), GUADALAJARA(20), GUIPUZCOA(21), HUELVA(22), HUESCA(23), ISLAS_BALEARES(24),
+    JAEN(25), LA_RIOJA(26), LAS_PALMAS(27), LEON(28), LLEIDA(29), LUGO(30), MADRID(31),
+    MALAGA(32), MELILLA(33), MURCIA(34), NAVARRA(35), OURENSE(36), PALENCIA(37),
+    PONTEVEDRA(38), SALAMANCA(39), SANTA_CRUZ_DE_TENERIFE(40), SEGOVIA(41),
+    SEVILLA(42), SORIA(43), TARRAGONA(44), TERUEL(45), TOLEDO(46), VALENCIA(47),
+    VALLADOLID(48), VIZCAYA(49), ZAMORA(50), ZARAGOZA(51);
 
     companion object {
         fun fromString(name: String): SpanishProvinces? {
             return values().find { it.name.equals(name, ignoreCase = true) }
         }
+
+        fun fromId(id: Int): SpanishProvinces? {
+            return values().find { it.id == id }
+        }
     }
 }
 
-enum class PortugueseRegions {
-    AVEIRO, BEJA, BRAGA, BRAGANCA, CASTELO_BRANCO, COIMBRA,
-    EVORA, FARO, GUARDA, LEIRIA, LISBOA, PORTALEGRE, PORTO,
-    SANTAREM, SETUBAL, VIANA_DO_CASTELO, VILA_REAL, VISEU,
-    REGIAO_AUTNOMA_DE_AZORES, REGIAO_AUTNOMA_DE_MADEIRA;
+enum class PortugueseRegions(val id: Int) {
+    AVEIRO(1), BEJA(2), BRAGA(3), BRAGANCA(4), CASTELO_BRANCO(5), COIMBRA(6),
+    EVORA(7), FARO(8), GUARDA(9), LEIRIA(10), LISBOA(11), PORTALEGRE(12), PORTO(13),
+    SANTAREM(14), SETUBAL(15), VIANA_DO_CASTELO(16), VILA_REAL(17), VISEU(18),
+    REGIAO_AUTNOMA_DE_AZORES(19), REGIAO_AUTNOMA_DE_MADEIRA(20);
 
     companion object {
         fun fromString(name: String): PortugueseRegions? {
             return values().find { it.name.equals(name, ignoreCase = true) }
         }
+
+        fun fromId(id: Int): PortugueseRegions? {
+            return values().find { it.id == id }
+        }
     }
 }
 
-enum class FrenchRegions {
-    ALSACIA, AQUITANIA, AUVERNA_RODANO_ALPES, BRETAA, BORGAA_FRANCO_CONDADO,
-    CENTRO_VALLE_DE_LOIRA, CHAMPAGNE_ARDENAS, CORSA, GRAN_ESTE, NORMANDA,
-    NUEVA_AQUITANIA, OCCITANIA, PAYS_DE_LA_LOIRE, PROVENZA_ALPES_COTA_AZUL,
-    ISLA_DE_FRANCIA, GUYANA_FRANCES, GUADALUPE, MARTINICA, REUNIN, MAYOTTE;
+enum class FrenchRegions(val id: Int) {
+    ALSACIA(1), AQUITANIA(2), AUVERNA_RODANO_ALPES(3), BRETAA(4), BORGAA_FRANCO_CONDADO(5),
+    CENTRO_VALLE_DE_LOIRA(6), CHAMPAGNE_ARDENAS(7), CORSA(8), GRAN_ESTE(9), NORMANDA(10),
+    NUEVA_AQUITANIA(11), OCCITANIA(12), PAYS_DE_LA_LOIRE(13), PROVENZA_ALPES_COTA_AZUL(14),
+    ISLA_DE_FRANCIA(15), GUYANA_FRANCES(16), GUADALUPE(17), MARTINICA(18), REUNIN(19), MAYOTTE(20);
 
     companion object {
         fun fromString(name: String): FrenchRegions? {
             return values().find { it.name.equals(name, ignoreCase = true) }
+        }
+
+        fun fromId(id: Int): FrenchRegions? {
+            return values().find { it.id == id }
         }
     }
 }
