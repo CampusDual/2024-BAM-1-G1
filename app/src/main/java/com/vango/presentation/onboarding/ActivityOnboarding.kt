d86d423eda588b389ffcb3db2e5173431caaeb0d
DESKTOP-2CUV67N\Lorena <lorenalopezvazquez@hotmail.com>
2025-03-05 23:55:54 +0100

    feat: add onboarding flow with animations and dynamic content
    This commit introduces the onboarding flow to the app.
    - Creates `Animations` utility class to handle width change animation.
    - Implements `ActivityOnboarding` to display three onboarding screens.
    - Adds `AdapterOnboarding` to manage the screens in a `ViewPager2`.
    - Updates indicators and their animations based on the current screen.
    - Updates title and description dynamically for each screen.
    - Creates `ActivityOnboardingViewModel` to handle onboarding status.
    - Implements logic to mark onboarding as completed after the last screen.
    - Adds a close button to skip onboarding and navigate to the access account screen.
    - Adds a next button to navigate through the onboarding flow.
    - Modifies layout files for the onboarding screen and bullets indicators.
    - Updates strings.xml for onboarding content.
    - Removes old, unused Activity files for each onboarding screen.
    - Modifies AndroidManifest.xml to remove old onboarding activities and update `ActivityOnboarding` configuration.

--- app/src/main/java/com/vango/presentation/onboarding/ActivityOnboarding.kt
+++ app/src/main/java/com/vango/presentation/onboarding/ActivityOnboarding.kt
@@ -6,19 +6,23 @@ import android.os.Bundle
 import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
 import com.google.firebase.auth.FirebaseAuth
+import com.vango.R
 import com.vango.data.preferences.OnboardingPreferences
 import com.vango.databinding.ActivityOnboardingBinding
 import com.vango.databinding.FragmentOnboardingBinding
 import com.vango.presentation.auth.accessAccount.ActivityAccessAccount
 import com.vango.presentation.base.BaseActivity
 import com.vango.presentation.main.ActivityMain
+import com.vango.utils.animation.Animations
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class ActivityOnboarding : BaseActivity() {
-    private var binding: ActivityOnboardingBinding? = null
+class ActivityOnboarding : AppCompatActivity() {
+    private lateinit var binding: ActivityOnboardingBinding
     private val viewModel: ActivityOnboardingViewModel by viewModels()
     private var oldView: View? = null
 
@@ -56,12 +60,8 @@ class ActivityOnboarding : BaseActivity() {
         enableEdgeToEdge()
         binding = ActivityOnboardingBinding.inflate(layoutInflater)
         setContentView(binding?.root)
-//        val btnNext = binding?.ivArrowRight
 
-//        btnNext?.setOnClickListener{
-//            val intent = Intent(this, ActivityOnboardingTwo::class.java)
-//            startActivity(intent)
-//        }
+        updateTitleAndDescription(0)
 
     }
     private fun isFirstLaunch(context: Context): Boolean {
@@ -76,9 +76,83 @@ class ActivityOnboarding : BaseActivity() {
         return false
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        binding = null
+    private fun setOnboarding() {
+        val onboardingAdapter = AdapterOnboarding(this)
+
+        binding?.viewPager?.adapter = onboardingAdapter
+        updateIndicators(0)
+
+        binding.viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                updateIndicators(position)
+                updateTitleAndDescription(position)
+
+                if (position == onboardingAdapter.itemCount - 1) {
+                    viewModel.markOnboardingAsCompleted()
+                }
+            }
+        })
+
+        binding.btnClose.setOnClickListener {
+            onboardingPreferences.setOnboardingCompleted(true)
+            val intent = Intent(this, ActivityAccessAccount::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        binding.btnNext.setOnClickListener {
+            var currentItem = binding.viewPager.currentItem
+            if (currentItem < onboardingAdapter.itemCount - 1) {
+                currentItem +=1
+            }else {
+                onboardingPreferences.setOnboardingCompleted(true)
+                val intent = Intent(this, ActivityAccessAccount::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+
+
     }
 
+    private fun updateIndicators(position: Int) {
+        binding.indicator1.isSelected = position == 0
+        binding.indicator2.isSelected = position == 1
+        binding.indicator3.isSelected = position == 2
+
+        oldView?.let {
+            Animations.animateWidthChange(it,30, 15,400)
+        }
+
+        oldView = when (position) {
+            0 -> binding.indicator1
+            1 -> binding.indicator2
+            2 -> binding.indicator3
+            else -> null
+        }
+
+        oldView?.let {
+            Animations.animateWidthChange(it,30, 15,400)
+        }
+    }
+
+    private fun updateTitleAndDescription(position: Int){
+        when(position){
+            0 -> {
+                binding.tvOnboardingTitle.text = getString(R.string.onboarding_one_title)
+                binding.tvOnboardingBody.text = getString(R.string.onboarding_one_body)
+            }
+
+            1 -> {
+                binding.tvOnboardingTitle.text = getString(R.string.onboarding_two_title)
+                binding.tvOnboardingBody.text = getString(R.string.onboarding_two_body)
+            }
+
+            2 -> {
+                binding.tvOnboardingTitle.text = getString(R.string.onboarding_three_title)
+                binding.tvOnboardingBody.text = getString(R.string.onboarding_three_body)
+            }
+        }
+    }
 }
--- app/src/main/java/com/vango/presentation/onboarding/ActivityOnboardingViewModel.kt
+++ app/src/main/java/com/vango/presentation/onboarding/ActivityOnboardingViewModel.kt
@@ -3,7 +3,10 @@ package com.vango.presentation.onboarding
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.vango.data.preferences.OnboardingPreferences
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -11,5 +14,17 @@ import javax.inject.Inject
 class ActivityOnboardingViewModel @Inject constructor(): ViewModel(){
     private var _showOnBoarding = MutableLiveData<Boolean>()
     var showOnBoarding : LiveData<Boolean> = _showOnBoarding
+    @Inject
+    lateinit var onboardingPreferences: OnboardingPreferences
+    init {
+        viewModelScope.launch {
+            _showOnBoarding.value = onboardingPreferences.isOnboardingCompleted()
+        }
+    }
 
+    fun markOnboardingAsCompleted() {
+        viewModelScope.launch {
+            onboardingPreferences.setOnboardingCompleted(true)
+        }
+    }
 }
