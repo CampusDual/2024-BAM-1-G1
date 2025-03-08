6f210a1a1a1fed9133d4994fe6ae5932bb5e1866
JamirGDC <gutierrez.dc@icloud.com>
2025-03-08 22:36:44 +0100

    refactor: redesign access account screen and update UI elements
    - Redesigns the `ActivityAccessAccount` layout for a more modern look.
    - Removes the loading animation and Google/Outlook buttons from `ActivityAccessAccount`.
    - Adds a ViewPager2 for image display in `ActivityAccessAccount`.
    - Adds a close button and mask to the image area in `ActivityAccessAccount`.
    - Adds text and button to the lower container in `ActivityAccessAccount`.
    - Moves the login link to the bottom of the screen in `ActivityAccessAccount`.
    - Changes button colors to use `color_main` and `deactivated_color` in `ActivityProfile` and `rounded_clear_button.xml`.
    - Adjusts margins and padding in `activity_onboarding.xml`.
    - Updates the path data for rounded bottom shape in `rounded_bottom.xml`.

--- app/src/main/java/com/vango/presentation/main/home/HomeScreen.kt
+++ app/src/main/java/com/vango/presentation/main/home/HomeScreen.kt
@@ -1,17 +1,29 @@
 package com.vango.presentation.main.home
 
 import android.Manifest
-import android.location.Location
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.slideInVertically
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.ExitToApp
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -21,10 +33,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
@@ -45,6 +61,9 @@ import com.google.maps.android.compose.MapUiSettings
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
+import com.vango.R
+import com.vango.presentation.theme.BackgroundButtonColor
+import com.vango.presentation.theme.BackgroundColorBadge
 
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
@@ -93,8 +112,6 @@ fun HomeScreen(
 
     fusedLocationClient.requestLocationUpdates(locationRequest, locationListener, null)
 
-
-
     fun moveYourLocation() {
         if (locationPermission.status.isGranted) {
             fusedLocationClient.lastLocation
@@ -109,7 +126,6 @@ fun HomeScreen(
             locationPermission.launchPermissionRequest()
         }
     }
-
 
     if (isPreview) {
         Box(
@@ -143,7 +159,7 @@ fun HomeScreen(
             }
 
             Button(
-                onClick = { navController.navigate("results")},
+                onClick = { navController.navigate("results") },
                 modifier = Modifier
                     .align(Alignment.BottomCenter)
                     .wrapContentWidth()
@@ -152,20 +168,13 @@ fun HomeScreen(
                 Text("Lista")
             }
 
-            // Button to navigate to the results screen
-            Button(
-                onClick = { moveYourLocation() },
+            FilterButton(
+                onMoveYourLocation = { moveYourLocation() }, // Pasa la función
                 modifier = Modifier
-                    .align(Alignment.BottomStart)
-                    .wrapContentWidth()
-                    .padding(16.dp)
-            ) {
-                Icon(
-                    imageVector = Icons.Default.ExitToApp,
-                    contentDescription = "Stop"
-                )
-            }
-            // button to add a new location
+                    .align(Alignment.TopStart)
+                    .padding(top = 48.dp, start = 20.dp)
+            )
+
             Button(
                 onClick = { navController.navigate("results") },
                 modifier = Modifier
@@ -178,8 +187,230 @@ fun HomeScreen(
                     contentDescription = "Add"
                 )
             }
+        }
+    }
+}
+
+@Composable
+fun FilterButton(
+    onMoveYourLocation: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    var isExpanded by remember { mutableStateOf(false) }
+    var isCampingSelected by remember { mutableStateOf(false) }
+    var isParkingSelected by remember { mutableStateOf(false) }
+    var isHospitalSelected by remember { mutableStateOf(false) }
+    var isFuelStationSelected by remember { mutableStateOf(false) }
+    var isLaundrySelected by remember { mutableStateOf(false) }
+
+    val selectedCount = listOf(
+        isCampingSelected,
+        isParkingSelected,
+        isHospitalSelected,
+        isFuelStationSelected,
+        isLaundrySelected
+    ).count { it }
+
+    Box(
+        modifier = Modifier
+            .clickable(
+                enabled = isExpanded,
+                onClick = { isExpanded = false },
+                indication = null,
+                interactionSource = remember { MutableInteractionSource() }
+            )
+    ) {
+
+        Box {
+            Surface(
+                onClick = { isExpanded = !isExpanded },
+                modifier = modifier
+                    .width(60.dp)
+                    .height(60.dp)
+                    .shadow(elevation = 4.dp, shape = RoundedCornerShape(16.dp)),
+
+                shape = RoundedCornerShape(16.dp),
+                color = BackgroundButtonColor
+            ) {
+                Column(
+                    modifier = Modifier.padding(4.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Icon(
+                        painter = if (isExpanded) painterResource(id = R.drawable.filter_no_fill) else painterResource(
+                            id = R.drawable.filter_fill
+                        ),
+                        contentDescription = "Filtros",
+                        modifier = Modifier.size(25.dp),
+                        tint = Color.White
+                    )
+                    Text(
+                        text = "Filtros",
+                        fontSize = 10.sp,
+                        color = Color.White
+                    )
+                }
+            }
+
+            if (selectedCount > 0) {
+                Surface(
+                    modifier = Modifier
+                        .align(Alignment.TopEnd)
+                        .padding(top = 40.dp, start = 67.dp)
+                        .size(22.dp)
+                        .shadow(elevation = 2.dp, shape = CircleShape),
+                    shape = CircleShape,
+                    color = BackgroundColorBadge
+                ) {
+                    Text(
+                        text = selectedCount.toString(),
+                        color = Color.White,
+                        fontSize = 15.sp,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.padding(2.dp)
+                    )
+                }
+            }
+
+            AnimatedVisibility(
+                visible = isExpanded,
+                enter = fadeIn(animationSpec = tween(durationMillis = 300)),
+                modifier = Modifier
+                    .align(Alignment.TopStart)
+                    .padding(top = 115.dp, start = 20.dp)
+                    .width(60.dp)
+                    .background(Color.Transparent)
+            ) {
+                Column(
+                    verticalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    AnimatedVisibility(
+                        visible = isExpanded,
+                        enter = slideInVertically(
+                            initialOffsetY = { -it },
+                            animationSpec = tween(durationMillis = 300, delayMillis = 0)
+                        ) + fadeIn(animationSpec = tween(durationMillis = 300, delayMillis = 0))
+                    ) {
+                        FilterOption(
+                            text = "Camping",
+                            painter = if (isCampingSelected) painterResource(id = R.drawable.camper_fill) else painterResource(
+                                id = R.drawable.camper_no_fill
+                            ),
+                            isSelected = isCampingSelected,
+                            onClick = { isCampingSelected = !isCampingSelected }
+                        )
+                    }
+
+                    AnimatedVisibility(
+                        visible = isExpanded,
+                        enter = slideInVertically(
+                            initialOffsetY = { -it },
+                            animationSpec = tween(durationMillis = 300, delayMillis = 100)
+                        ) + fadeIn(animationSpec = tween(durationMillis = 300, delayMillis = 100))
+                    ) {
+                        FilterOption(
+                            text = "Parking",
+                            painter = if (isParkingSelected) painterResource(id = R.drawable.parking_fill) else painterResource(
+                                id = R.drawable.parking_no_fill
+                            ),
+                            isSelected = isParkingSelected,
+                            onClick = { isParkingSelected = !isParkingSelected }
+                        )
+                    }
+
+                    AnimatedVisibility(
+                        visible = isExpanded,
+                        enter = slideInVertically(
+                            initialOffsetY = { -it },
+                            animationSpec = tween(durationMillis = 300, delayMillis = 200)
+                        ) + fadeIn(animationSpec = tween(durationMillis = 300, delayMillis = 200))
+                    ) {
+                        FilterOption(
+                            text = "Hospital",
+                            painter = if (isHospitalSelected) painterResource(id = R.drawable.hospital_fill) else painterResource(
+                                id = R.drawable.hospital_no_fill
+                            ),
+                            isSelected = isHospitalSelected,
+                            onClick = { isHospitalSelected = !isHospitalSelected }
+                        )
+                    }
+
+                    AnimatedVisibility(
+                        visible = isExpanded,
+                        enter = slideInVertically(
+                            initialOffsetY = { -it },
+                            animationSpec = tween(durationMillis = 300, delayMillis = 200)
+                        ) + fadeIn(animationSpec = tween(durationMillis = 300, delayMillis = 200))
+                    ) {
+                        FilterOption(
+                            text = "Gasolinera",
+                            painter = if (isFuelStationSelected) painterResource(id = R.drawable.fuel_station_fill) else painterResource(
+                                id = R.drawable.fuel_station_no_fill
+                            ),
+                            isSelected = isFuelStationSelected,
+                            onClick = { isFuelStationSelected = !isFuelStationSelected }
+                        )
+                    }
+                    AnimatedVisibility(
+                        visible = isExpanded,
+                        enter = slideInVertically(
+                            initialOffsetY = { -it },
+                            animationSpec = tween(durationMillis = 300, delayMillis = 200)
+                        ) + fadeIn(animationSpec = tween(durationMillis = 300, delayMillis = 200))
+                    ) {
+                        FilterOption(
+                            text = "Lavanderia",
+                            painter = if (isLaundrySelected) painterResource(id = R.drawable.laundry_fill) else painterResource(
+                                id = R.drawable.laundry_no_fill
+                            ),
+                            isSelected = isLaundrySelected,
+                            onClick = { isLaundrySelected = !isLaundrySelected }
+                        )
+                    }
+                }
+            }
+        }
+
+    }
 
 
+}
+
+@Composable
+fun FilterOption(
+    text: String,
+    painter: androidx.compose.ui.graphics.painter.Painter,
+    isSelected: Boolean,
+    onClick: () -> Unit
+) {
+    Surface(
+        onClick = onClick,
+        modifier = Modifier
+            .width(60.dp)
+            .height(60.dp)
+            .shadow(elevation = 4.dp, shape = RoundedCornerShape(16.dp)),
+        shape = RoundedCornerShape(16.dp),
+        color = if (isSelected) BackgroundButtonColor else Color.Gray
+    ) {
+        Column(
+            modifier = Modifier.padding(4.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Icon(
+                painter = painter,
+                contentDescription = text,
+                modifier = Modifier.size(25.dp),
+                tint = Color.White
+            )
+
+            Text(
+                text = text,
+                modifier = Modifier.padding(top = 3.dp),
+                fontSize = 10.sp,
+                color = Color.White
+            )
         }
     }
 }
--- app/src/main/java/com/vango/presentation/theme/Color.kt
+++ app/src/main/java/com/vango/presentation/theme/Color.kt
@@ -9,3 +9,6 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val BackgroundButtonColor = Color(0xFF3CA6A5)
+val BackgroundColorBadge = Color(0xFFE75E5F)
