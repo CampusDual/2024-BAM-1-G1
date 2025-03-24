35803f4f6402b20b02e7a60fd08d47a2b6d49a1b
zYoelz <vip.yoel@gmail.com>
2025-03-24 09:51:34 +0100

    feat: Improve help screen and add warning color
    - Adds `WarningColor` to the theme for better visual indication.
    - Updates the `RecommendationCard` to display a warning icon and use `WarningColor` for the title.
    - Modifies `RecommendationCard` to allow to text parts on action button.
    - Adds `ExpandableFAQItem` to display questions and answers, and removes the previous `FAQItem`.
    - Introduces `ic_warning.xml` as a new drawable resource for warning icons.
    - Adds logic to show/hide the FAQ answers.
    -Updates faqs answers.
    -Adds a spacer to save the bottombar.
    - Removes unused import `com.google.rpc.Help` in Help.kt.

--- app/src/main/java/com/vango/presentation/main/menu/support/Help.kt
+++ app/src/main/java/com/vango/presentation/main/menu/support/Help.kt
@@ -16,19 +16,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredWidth
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
@@ -40,10 +37,10 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import com.google.rpc.Help
 import com.vango.R
 import com.vango.presentation.theme.MainColor
 import com.vango.presentation.theme.TextColor
+import com.vango.presentation.theme.WarningColor
 
 @Composable
 fun HelpScreen(navController: NavController) {
@@ -89,7 +86,8 @@ fun HelpScreen(navController: NavController) {
                 RecommendationCard(
                     title = "Tú identidad no se ha verificado",
                     description = "Verificar tu identidad nos ayuda a confirmar que eres tú. Es por ello que es muy importante que vayas a tu cuenta y rellenes todos los datos.",
-                    actionText = "Ir a Mi cuenta",
+                    actionText1 = "Ir a ",
+                    actionText2 = "Mi cuenta",
                     onActionClick = { navController.navigate("home") }
                 )
             }
@@ -105,20 +103,102 @@ fun HelpScreen(navController: NavController) {
         )
 
         val faqs = listOf(
-            "¿Qué es esta aplicación y para qué sirve?" to "faq_1",
-            "¿En qué dispositivos puedo usar la APP?" to "faq_2",
-            "¿La aplicación es gratuita?" to "faq_3",
-            "Olvidé mi contraseña, ¿cómo la recupero?" to "faq_4",
-            "¿La app muestra información sobre normativas de estacionamiento?" to "faq_5",
-            "¿Cómo reporto un lugar incorrecto o con información desactualizada?" to "faq_6"
+            "¿Qué es esta aplicación y para qué sirve?" to "Esta aplicacion te salvara la vida cuando viajes en Camper, sera tu ilusion, te sentiras desnudo sin ella cuando salgas de casa y creeras que estan perdido en la selva sin ella.",
+            "¿En qué dispositivos puedo usar la APP?" to "Desde un pisapapeles hasta esos preciosos HONOR Magic V3 , pero por ahora no para los sobrevalorados y supercaros Iphons.",
+            "¿La aplicación es gratuita?" to "Desearias pagar por esta droga todo tu dinero, por ahora esta en modo de campaña y es gratis pero pronto pondremos la manera de sacasrte todo tu dinero",
+            "Olvidé mi contraseña, ¿cómo la recupero?" to "Create otro email, y otra cuanta, pon tu targeta otra vez y hazte de la cuanta premiun otra vez, nosotros si sabemos usar cotrrectamente tu dinero..",
+            "¿La app muestra información sobre normativas de estacionamiento?" to "Que va, si te multan paga, nosotros no nos hacemos responsables de que no sepas donde esta parado y te aparques al lado de una señal de prohivido parar y estacionar.",
+            "¿Cómo reporto un lugar incorrecto o con información desactualizada?" to "Manda un correo a meimportaunpepinoloquedigas@miscojonesmaduros.orto."
         )
 
-        faqs.forEach { (question, route) ->
-            FAQItem(
+        faqs.forEach { (question, answer) ->
+            ExpandableFAQItem(
                 question = question,
-                onClick = { navController.navigate(route) }
+                answer = answer
             )
         }
+        Spacer(modifier = Modifier.height(85.dp))// para salvar la bottombar
+    }
+}
+
+@Composable
+fun ExpandableFAQItem(question: String, answer: String) {
+    val isExpanded =
+        remember { mutableStateOf(false) } // Estado para controlar la visibilidad de la respuesta
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp)
+    ) {
+        // Pregunta clickeable
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable { isExpanded.value = !isExpanded.value }
+                .padding(vertical = 12.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth(0.9f),
+                contentAlignment = Alignment.CenterStart
+            ) {
+                Text(
+                    text = question,
+                    fontSize = 14.sp,
+                    color = TextColor,
+                    fontWeight = FontWeight.Normal
+                )
+            }
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                contentAlignment = Alignment.CenterEnd
+            ) {
+                if (isExpanded.value) {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_edit_password),
+                        contentDescription = "Atras",
+                        modifier = Modifier
+                            .width(10.dp)
+                            .height(10.dp)
+                            .rotate(-90f),
+                        colorFilter =
+                        androidx.compose.ui.graphics.ColorFilter.tint(com.vango.presentation.theme.TextColor)
+                    )
+                } else {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_edit_password),
+                        contentDescription = "Atras",
+                        modifier = Modifier
+                            .width(10.dp)
+                            .height(10.dp)
+                            .rotate(90f),
+                        colorFilter =
+                        androidx.compose.ui.graphics.ColorFilter.tint(com.vango.presentation.theme.TextColor)
+                    )
+                }
+
+            }
+        }
+
+        // Respuesta (visible solo si isExpanded es true)
+        if (isExpanded.value) {
+            Text(
+                text = answer,
+                fontSize = 14.sp,
+                color = TextColor.copy(alpha = 0.7f),
+                modifier = Modifier.padding(start = 16.dp, bottom = 8.dp)
+            )
+        }
+
+        // Divisor
+        HorizontalDivider(
+            modifier = Modifier.padding(horizontal = 0.dp),
+            thickness = 1.dp,
+            color = Color.Gray.copy(alpha = 0.2f)
+        )
     }
 }
 
@@ -126,7 +206,8 @@ fun HelpScreen(navController: NavController) {
 fun RecommendationCard(
     title: String,
     description: String,
-    actionText: String,
+    actionText1: String,
+    actionText2: String,
     onActionClick: () -> Unit
 ) {
     val CardBackgroundColor = Color.White
@@ -142,18 +223,19 @@ fun RecommendationCard(
     ) {
         Column {
             Row(verticalAlignment = Alignment.CenterVertically) {
-                Icon(
-                    imageVector = Icons.Default.Warning,
-                    contentDescription = null,
-                    tint = Color.Red,
-                    modifier = Modifier.size(18.dp)
+                Image(
+                    painter = painterResource(id = R.drawable.ic_warning),
+                    contentDescription = "Warning",
+                    modifier = Modifier
+                        .width(18.dp)
+                        .height(18.dp)
                 )
                 Spacer(modifier = Modifier.width(8.dp))
                 Text(
                     text = "Accion Necesaria",
                     fontSize = 14.sp,
                     fontWeight = FontWeight.Bold,
-                    color = Color.Red
+                    color = WarningColor
                 )
             }
             Spacer(modifier = Modifier.height(18.dp))
@@ -166,7 +248,7 @@ fun RecommendationCard(
             Spacer(modifier = Modifier.height(18.dp))
             Text(
                 text = description,
-                fontSize = 12.sp,
+                fontSize = 14.sp,
                 color = TextColor
             )
             Box(
@@ -186,13 +268,24 @@ fun RecommendationCard(
                                 .fillMaxWidth(0.8f),
                             contentAlignment = Alignment.CenterStart
                         ) {
-                            Text(
-                                text = actionText,
-                                fontSize = 14.sp,
-                                color = TextColor,
-                                fontWeight = FontWeight.Bold,
-                                modifier = Modifier.clickable(onClick = onActionClick)
-                            )
+                            Row {
+                                Text(
+                                    text = actionText1,
+                                    fontSize = 14.sp,
+                                    color = TextColor,
+                                    fontWeight = FontWeight.Normal,
+                                    modifier = Modifier.clickable(onClick = onActionClick)
+                                )
+                                Text(
+                                    text = actionText2,
+                                    fontSize = 14.sp,
+                                    color = TextColor,
+                                    fontWeight = FontWeight.Bold,
+                                    modifier = Modifier.clickable(onClick = onActionClick)
+                                )
+
+                            }
+
                         }
 
                         Box(
@@ -219,53 +312,6 @@ fun RecommendationCard(
     }
 }
 
-@Composable
-fun FAQItem(question: String, onClick: () -> Unit) {
-    val TextColor = Color.Black
-
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .clickable(onClick = onClick)
-            .padding(vertical = 12.dp),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Box(
-            modifier = Modifier
-                .fillMaxWidth(0.8f),
-            contentAlignment = Alignment.CenterStart
-        ) {
-            Text(
-                text = question,
-                fontSize = 12.sp,
-                color = TextColor,
-                fontWeight = FontWeight.Normal
-            )
-        }
-
-        Box(
-            modifier = Modifier
-                .fillMaxWidth(),
-            contentAlignment = Alignment.CenterEnd
-        ) {
-            Image(
-                painter = painterResource(id = R.drawable.ic_edit_password),
-                contentDescription = "Atras",
-                modifier = Modifier
-                    .width(10.dp)
-                    .height(10.dp)
-                    .rotate(90f),
-                colorFilter = androidx.compose.ui.graphics.ColorFilter.tint(com.vango.presentation.theme.TextColor)
-            )
-        }
-        Spacer(modifier = Modifier.height(24.dp))
-    }
-    HorizontalDivider(
-        modifier = Modifier.padding(horizontal = 0.dp),
-        thickness = 1.dp,
-        color = Color.Gray.copy(alpha = 0.2f)
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
